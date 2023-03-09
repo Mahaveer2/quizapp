c16343f276190690f821d62b33422a48dc1f7940
Partab Rai <amrani.partab@gmail.com>
2023-03-09 12:58:17 +0500

    changes

--- src/routes/admin/+layout.svelte
+++ src/routes/admin/+layout.svelte
@@ -1,11 +1,14 @@
 <script>
 	import Sidebar from "./components/Sidebar.svelte"
   import { page } from "$app/stores";
+	import Transition from "$lib/components/Transition.svelte"
 </script>
 {#if $page.data.admin}
 <Sidebar/>
 <div class="lg:w-[calc(100% - 300px)] w-[calc(100% - 90px)] lg:ml-[300px] ml-[90px] bg-white h-[100vh] ">
-  <slot/>
+    <slot/>
 </div>
+{:else}
+<slot/>
 {/if}
 
