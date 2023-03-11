41c07a997599e7d6ea353c61caf18383cbaa4252
Partab Rai <amrani.partab@gmail.com>
2023-03-11 10:23:07 +0500

    debug env

--- src/routes/account/+page.server.ts
+++ src/routes/account/+page.server.ts
@@ -39,7 +39,7 @@ const pay: Action = async ({ request, cookies }) => {
 		client_secret: VITE_PAYPAL_SECRET
 	})
 
-	const siteUrl = DEBUG ? 'http://localhost:5173/' : DOMAIN
+	const siteUrl = DEBUG == 'true' ? 'http://localhost:5173/' : DOMAIN
 
 	const create_payment_json = {
 		intent: 'sale',
