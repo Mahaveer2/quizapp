9bbb837f976e44e9c41dbba3e6907d8e3026e7a6
Partab Rai <amrani.partab@gmail.com>
2023-03-16 19:10:51 +0500

    changes

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,18 @@
 import preprocess from "svelte-preprocess";
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit//vite';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 
-// import adapter from '@sveltejs/adapter-netlify';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: [vitePreprocess(), preprocess({
-		postcss: true
-})],
-  kit: {
-    adapter: adapter({})
-  }
+        postcss: true
+    })],
+
+	kit: {
+		adapter: adapter({})
+	}
 };
 
 export default config;
