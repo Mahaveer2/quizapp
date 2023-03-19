a78acf3a9e45cb1fdf4c79ff76a3046fbea07028
Partab Rai <amrani.partab@gmail.com>
2023-03-19 20:27:53 +0500

    changes

--- src/routes/api/chat/+server.ts
+++ src/routes/api/chat/+server.ts
@@ -20,18 +20,24 @@ export const POST: RequestHandler = async ({ request }) => {
 			'You are a study helper chat bot your name is profbot and your goal is to help users learn;if by chance any one asks for quiz him or test tell him to go to navigation and see a link called test;if anyone asks to quiz me tell him about the navigation link if he stills asks to quiz create a quiz'
 		const chatRequestOpts: CreateChatCompletionRequest = {
 			model: 'gpt-3.5-turbo',
-			messages: [{ 
-				role: 'system', content: prompt 
-			},{
-				role:'system',
-				content:"If user asks for quiz you give him a link : <a href='/test'>The tests are here</>"
-			}, ...messages],
+			messages: [
+				{
+					role: 'system',
+					content: prompt
+				},
+				{
+					role: 'system',
+					content:
+						"If user asks for quiz or says to test him about a topic you should give him a link : <a href='/test'>The tests are here</>"
+				},
+				...messages
+			],
 			temperature: 1,
 			stream: true
 		}
-    let timeTakento :number = 0;
+		let timeTakento: number = 0
 
-    setInterval(() => timeTakento++,1000)
+		setInterval(() => timeTakento++, 1000)
 
 		const { body } = await fetch('https://api.openai.com/v1/chat/completions', {
 			headers: {
@@ -41,7 +47,7 @@ export const POST: RequestHandler = async ({ request }) => {
 			method: 'POST',
 			body: JSON.stringify(chatRequestOpts)
 		})
-    console.log(timeTakento)
+		console.log(timeTakento)
 
 		return new Response(body, {
 			headers: {
--- src/routes/test/[link]/+page.svelte
+++ src/routes/test/[link]/+page.svelte
@@ -186,7 +186,7 @@
 </script>
 
 	<div class="flex flex-col pt-4 w-full items-center absolute top-[45px]">
-		<h1 class="my-4 text-sm absolute top-2 bg-yellow-500 p-3 rounded-full">
+		<h1 class="my-4 text-sm absolute top-2 bg-yellow-500 p-3 rounded-full z-[22]">
 			{formatTime(timeLeft)} left
 		</h1>
 		<div class="h-[63vh] w-full p-4 overflow-y-auto flex flex-col gap-4">
