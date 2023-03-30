8fb43b7a5f0856eeb9f3b05a0d6cc4203cdb13a3
Partab Rai <amrani.partab@gmail.com>
2023-03-31 00:24:11 +0500

    new sessions

--- src/routes/admin/+page.server.ts
+++ src/routes/admin/+page.server.ts
@@ -4,7 +4,7 @@ export async function load(){
   const students = await client.student.findMany();
   // const profCount = await client.admin.count();
   const testCount = await client.test.count();
-  const sessions = await client.paymentSessions.findMany();
+  const sessions = await client.creditsUsed.findMany();
   const cSession = await client.creditsUsed.findMany();
 
   return {
