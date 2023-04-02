29f664c3e8132a7f054ebd5d381813166fa9fb9b
Partab Rai <amrani.partab@gmail.com>
2023-04-02 22:14:28 +0500

    changes

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,12 +2,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 async function main(){
-  const tom = await prisma.admin.upsert({
-    where:{email:"tom@gmail.com"},
-    update:{},
-    create:{
-      email:'tom@gmail.com',
-      password:"admin"
-    }
-  })
+  const tom = await prisma.admin.findUnique({where:{email:"tom@gmail.com"}});
+
+  if(!tom){
+    await prisma.admin.create({
+      data:{
+        name:"Tom",
+        email:"tom@gmail.com",
+        password:"admin"
+      }
+    })
+  }
 }
